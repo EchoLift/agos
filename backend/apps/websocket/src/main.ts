da6eeb96f5942ac6aad1f1223129be89e93da03d
user.email <illasuryanani2001@gmail.com>
2026-08-10 21:05:10 +0530

    fixed render build

--- backend/apps/websocket/src/main.ts
+++ backend/apps/websocket/src/main.ts
@@ -5,7 +5,9 @@ import { WebsocketModule } from "./websocket.module";
 async function bootstrap() {
   const app = await NestFactory.create(WebsocketModule);
   const config = app.get(ConfigService);
-  const port = config.get<number>("WEBSOCKET_PORT") ?? 4001;
+  const port = Number(
+    config.get<string>("PORT") ?? config.get<string>("WEBSOCKET_PORT") ?? 4001,
+  );
   await app.listen(port);
 }
 
