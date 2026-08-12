23b8a0e53b602a9ec941300d6cb8618d3f9c8b51
EchoLift <illasuryanani2001@gmail.com>
2026-08-13 01:03:52 +0530

    Integrated email service

--- backend/modules/notification/notification.module.ts
+++ backend/modules/notification/notification.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { CryptoModule } from "@packages/crypto/crypto.module";
 import { NotificationService } from "./notification.service";
 import { EmailDeliveryService } from "./email/services/email-delivery.service";
 import { ResendProvider } from "./email/providers/resend.provider";
@@ -6,6 +7,7 @@ import { SendGridProvider } from "./email/providers/sendgrid.provider";
 import { NotificationDeliveryProcessor } from "./processors/notification-delivery.processor";
 
 @Module({
+  imports: [CryptoModule],
   providers: [
     NotificationService,
     EmailDeliveryService,
