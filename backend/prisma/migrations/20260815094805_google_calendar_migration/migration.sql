7a079213b98398c6ad60d5a3e74b152ed12b4dbe
EchoLift <illasuryanani2001@gmail.com>
2026-08-15 16:36:09 +0530

    fix duplicate notification index migration

--- backend/prisma/migrations/20260815094805_google_calendar_migration/migration.sql
+++ backend/prisma/migrations/20260815094805_google_calendar_migration/migration.sql
@@ -1,8 +1,3 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[invitationId,channel]` on the table `notification_deliveries` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "notification_deliveries_invitationId_channel_key" ON "notification_deliveries"("invitationId", "channel");
+-- Duplicate migration generated accidentally.
+-- The notification delivery unique index was already created by:
+-- 20260813000000_invitation_delivery_idempotency
